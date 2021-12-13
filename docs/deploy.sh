7ade4319d9ea1b0c4a2c4b4d06035b9a49a8a543
Parkjju <rudwns3927@gmail.com>
2021-12-13 18:10:16 +0900

    [chore] 에셋 파일 추가 / 알고리즘 문서화 관련 사진

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -12,7 +12,7 @@ cd src/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy with vuepress script'
-git push -f https://github.com/Parkjju/vue-TIL.git main:gh-pages
+git push -f https://github.com/Parkjju/vue-TIL.git master:gh-pages
 # 작업 환경따라서 main-master 변경하기
 # git push -f https://github.com/Parkjju/vuepressBlog.git/ main:gh-pages
 
