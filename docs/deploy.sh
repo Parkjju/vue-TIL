7b03bc21f63494a4c76a28e9ee82c44ae80b03e4
Parkjju <rudwns3927@gmail.com>
2022-01-12 23:37:39 +0900

    [chore] 구글 서치콘솔 등록, 메타태그 추가

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,16 +4,16 @@
 set -e
 
 # build commands
-yarn build
+npm run build
 
 # navigate into the build output directory
 cd src/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy with vuepress script'
+git commit -m "deploy with vuepress script"
 git push -f https://github.com/Parkjju/vue-TIL.git master:gh-pages
 # 작업 환경따라서 main-master 변경하기
-# git push -f https://github.com/Parkjju/vuepressBlog.git/ main:gh-pages
+# git push -f https://github.com/Parkjju/vue-TIL.git main:gh-pages
 
 cd -
