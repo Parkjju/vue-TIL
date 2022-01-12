dc9f8b925bb8159c8433064bb3144331815168a6
Parkjju <rudwns3927@gmail.com>
2022-01-12 14:54:35 +0900

    [docs] config.js head icon 경로수정, manifest.json 파일 수정

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
   head: [
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    ["link", { rel: "icon", href: "./assets/logo.png" }],
+    ["link", { rel: "icon", href: "./public/logo.png" }],
     ["link", { rel: "manifest", href: "./manifest.json" }],
     [
       "meta",
