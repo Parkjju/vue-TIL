b02cf65fd5eb9329719ccdd4e9c482cf4e23b9c5
Parkjju <rudwns3927@gmail.com>
2022-01-24 17:57:47 +0900

    [chore] 파비콘 웹 상에서 정상동작, 모바일 테스트 필요

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,51 +23,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/assets/favicons/apple-touch-icon.png",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/assets/favicons/favicon-32x32.png",
-      },
-    ],
-    [
-      "link",
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/assets/favicons/favicon-16x16.png",
-      },
-    ],
-    ["link", { rel: "manifest", href: "/assets/favicons/site.webmanifest" }],
-    [
-      "link",
-      {
-        rel: "mask-icon",
-        href: "/assets/favicons/safari-pinned-tab.svg",
-        color: "#3a0839",
-      },
-    ],
-    ["link", { rel: "shortcut icon", href: "/assets/favicons/favicon.ico" }],
-    ["meta", { name: "msapplication-TileColor", content: "#3a0839" }],
-    [
-      "meta",
-      {
-        name: "msapplication-config",
-        content: "/assets/favicons/browserconfig.xml",
-      },
-    ],
-    ["meta", { name: "theme-color", content: "#ffffff" }],
+    ["link", { rel: "shortcut icon", href: "./favicon.ico" }],
     ["meta", { name: "theme-color", content: "#3eaf7c" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["link", { rel: "icon", href: "./public/logo.png" }],
