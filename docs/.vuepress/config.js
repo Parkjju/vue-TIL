cc2c44f65903865945a1ce4d81f74bb0efdaaf56
Parkjju <rudwns3927@gmail.com>
2024-01-30 13:36:04 +0900

    docs: State

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -412,6 +412,7 @@ module.exports = {
                     'swiftui/240125-8',
                     'swiftui/240127-9',
                     'swiftui/240130-10',
+                    'swiftui/240130-10',
                 ],
                 sidebarDepth: 2,
             },
